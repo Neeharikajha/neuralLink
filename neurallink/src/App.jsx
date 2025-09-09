6162e58c481e51cf1ee09f85b5c469d189463e2d
Neeharikajha <neeharikajha05@gmail.com>
2025-09-10 00:55:01 +0530

    initialized frontend and backend

---
+++ neurallink/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen bg-pink-500 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-500 underline">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
